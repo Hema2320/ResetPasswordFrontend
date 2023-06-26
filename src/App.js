22b4fc9966773103d8f65dc489957439be7c21d6
Hema2320 <hemadharsh23@gmail.com>
2023-06-26 22:07:05 +0530

    ResetPassword Frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import ChangePassword from "./Pages/ChangePassword";
+import ForgetPassword from "./Pages/ForgetPassword";
+import Home from "./Pages/Home";
+import ProtectedRoutes from "./Services/ProtectedRoutes";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/reset-password" element={<ForgetPassword />} />
+          <Route path="/user/reset/:id/:token" element={<ChangePassword />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<ProtectedRoutes />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
